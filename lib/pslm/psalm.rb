7605d10361a50b841d6d6e253b85546e33907d5f
Jakub Pavlík jn <severus@post.cz>
2013-11-22 20:43:52 +0100

    crashing bug in Psalm#== fixed

--- lib/pslm/psalm.rb
+++ lib/pslm/psalm.rb
@@ -3,109 +3,115 @@
 require 'ostruct'
 
 # parsed psalm data
-class Pslm::Psalm
+module Pslm
+  class Psalm
 
-  def initialize
-    @header = OpenStruct.new
-    @header.title = ''
-    @verses = []
-    @strophes = []
-  end
-
-  attr_reader :header, :verses
-
-  def ==(ps2)
-    self.header == ps2.header && self.verses == ps2.verses
-  end
-
-  # returns a new Psalm containing verses of the second appended to the verses
-  # of the first; everything else is copied from the first
-  # (title etc. of the second Psalm get lost)
-  def +(ps2)
-    ps_res = self.dup
-    ps_res.verses.concat ps2.verses
-    return ps_res
-  end
-
-  class Verse
-    def initialize(flex=nil, first=nil, second=nil)
-      @flex = flex
-      @first = first
-      @second = second
-      if second.nil? then
-        yield self
-      end
+    def initialize
+      @header = OpenStruct.new
+      @header.title = ''
+      @verses = []
+      @strophes = []
     end
 
-    attr_accessor :flex, :first, :second
+    attr_reader :header, :verses
 
-    def parts
-      r = [@first, @second]
-      r.unshift @flex if @flex
-      return r
+    def ==(ps2)
+      ps2.is_a?(Psalm) && self.header == ps2.header && self.verses == ps2.verses
     end
 
-    def ==(v2)
-      [:flex, :first, :second].each do |part|
-        if self.send(part) != v2.send(part) then
-          return false
+    # returns a new Psalm containing verses of the second appended to the verses
+    # of the first; everything else is copied from the first
+    # (title etc. of the second Psalm get lost)
+    def +(ps2)
+      ps_res = self.dup
+      ps_res.verses.concat ps2.verses
+      return ps_res
+    end
+
+    class Verse
+      def initialize(flex=nil, first=nil, second=nil)
+        @flex = flex
+        @first = first
+        @second = second
+        if second.nil? then
+          yield self
         end
       end
 
-      return true
-    end
-  end
+      attr_accessor :flex, :first, :second
 
-  class VersePart
-    def initialize(words, src, pos)
-      @words = words
-      @src = src
-      @pos = pos
-    end
+      def parts
+        r = [@first, @second]
+        r.unshift @flex if @flex
+        return r
+      end
 
-    # Array of Words
-    attr_reader :words
+      def ==(v2)
+        unless v2.is_a? Verse
+          return false
+        end
 
-    # the source text before parsing
-    attr_reader :src
+        [:flex, :first, :second].each do |part|
+          if self.send(part) != v2.send(part) then
+            return false
+          end
+        end
 
-    # position in the verse - one of :flex, :first, :second
-    attr_reader :pos
-
-    def ==(p2)
-      self.words == p2.words
-    end
-  end
-
-  class Word
-    def initialize(syllables)
-      @syllables = syllables
+        return true
+      end
     end
 
-    attr_reader :syllables
+    class VersePart
+      def initialize(words, src, pos)
+        @words = words
+        @src = src
+        @pos = pos
+      end
 
-    def ==(w2)
-      self.syllables == w2.syllables
+      # Array of Words
+      attr_reader :words
+
+      # the source text before parsing
+      attr_reader :src
+
+      # position in the verse - one of :flex, :first, :second
+      attr_reader :pos
+
+      def ==(p2)
+        p2.is_a?(VersePart) && self.words == p2.words
+      end
     end
-  end
 
-  class Syllable < String
-    def initialize(chars, accent=false)
-      super(chars)
-      @accent = accent
+    class Word
+      def initialize(syllables)
+        @syllables = syllables
+      end
+
+      attr_reader :syllables
+
+      def ==(w2)
+        w2.is_a?(Word) && self.syllables == w2.syllables
+      end
     end
 
-    def accent?
-      @accent
-    end
+    class Syllable < String
+      def initialize(chars, accent=false)
+        super(chars)
+        @accent = accent
+      end
 
-    def ==(s2)
-      (self.to_s == s2.to_s) and (self.accent? == s2.accent?)
-    end
+      def accent?
+        @accent
+      end
 
-    def inspect
-      accent = @accent ? ' accent' : ''
-      return '#' + "<#{self.class}:#{self.object_id} \"#{self}\"#{accent}>"
+      def ==(s2)
+        s2.is_a? Syllable and (self.to_s == s2.to_s) and (self.accent? == s2.accent?)
+      end
+
+      def inspect
+        accent = @accent ? ' accent' : ''
+        return '#' + "<#{self.class}:#{self.object_id} \"#{self}\"#{accent}>"
+      end
     end
   end
 end
